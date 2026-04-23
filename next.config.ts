e30aab08a8d15f4f14857695822524c6b6403d1a
William Söderström <william.soderstrom30@gmail.com>
2026-04-23 10:31:10 +0200

    next config fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_VIEWER_URL: process.env.NEXT_PUBLIC_VIEWER_URL || "http://localhost:3001",
+  },
   async headers() {
     return [
       {
